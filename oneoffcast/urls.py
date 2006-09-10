4a7c75afa063dc4f598bda444524fc8c04c611c9
Doug Hellmann <doug@doughellmann.com>
2006-09-10 22:50:27 +0000

    Bad name
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@2 d63ba646-0c1d-0410-9968-bdc6b00dc62d

--- oneoffcast/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^oneoffcast/', include('oneoffcast.apps.foo.urls.foo')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
-)
