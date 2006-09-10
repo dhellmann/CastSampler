98f93e36bd98b4cff0ce7a06340e1eadc4f96a9a
Doug Hellmann <doug@doughellmann.com>
2006-09-10 22:52:16 +0000

    initial top level project
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@3 d63ba646-0c1d-0410-9968-bdc6b00dc62d

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^OneOffCast/', include('OneOffCast.apps.foo.urls.foo')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
