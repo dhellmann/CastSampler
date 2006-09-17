2a8e06443a07baf8d577030d52fa9110e936b8bf
Doug Hellmann <doug@doughellmann.com>
2006-09-17 02:13:07 +0000

    Sample enabled feed
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@21 d63ba646-0c1d-0410-9968-bdc6b00dc62d

--- trunk/urls.py
+++ trunk/urls.py
@@ -4,10 +4,16 @@
 
 from django.conf.urls.defaults import *
 
+#feeds = {
+#    'user':
+#    }
+
 urlpatterns = patterns('',
     # Example:
     # (r'^OneOffCast/', include('OneOffCast.apps.foo.urls.foo')),
 
     # Uncomment this for admin:
      (r'^admin/', include('django.contrib.admin.urls')),
+    
+#    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 )
