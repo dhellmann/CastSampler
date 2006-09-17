b35c9e643078ae5ad2cde88738234809548048db
Doug Hellmann <doug@doughellmann.com>
2006-09-17 14:39:20 +0000

    decouple urls a bit
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@29 d63ba646-0c1d-0410-9968-bdc6b00dc62d

---
+++ trunk/OneOffCast/oneoffcast/urls.py
@@ -0,0 +1,20 @@
+#
+# $Id$
+#
+
+from django.conf.urls.defaults import *
+from oneoffcast import feeds
+
+feeds = {
+    'atom':feeds.UserFeed,
+    }
+
+urlpatterns = patterns('',
+    # User feed
+    (r'^feed/(?P<url>.*)$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+) + \
+            patterns('OneOffCast.oneoffcast.views',
+
+    # User pages
+    (r'^(?P<user_id>.*)$', 'user'),
+    )
--- trunk/OneOffCast/urls.py
+++ trunk/OneOffCast/urls.py
@@ -3,23 +3,17 @@
 #
 
 from django.conf.urls.defaults import *
-from oneoffcast import feeds
-
-feeds = {
-    'atom':feeds.UserFeed,
-    }
 
 urlpatterns = patterns('',
     # Example:
     # (r'^OneOffCast/', include('OneOffCast.apps.foo.urls.foo')),
 
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
-    
     # Main page
     (r'^$', 'OneOffCast.oneoffcast.views.main'),
-    # User pages
-    (r'^cast/(?P<user_id>.*)$', 'OneOffCast.oneoffcast.views.user'),
-    # User feed
-    (r'^feed/(?P<url>.*)$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    
+    # Uncomment this for admin:
+     (r'^admin/', include('django.contrib.admin.urls')),
+
+    # The oneoffcast app
+    (r'^cast/', include('OneOffCast.oneoffcast.urls')),
 )
