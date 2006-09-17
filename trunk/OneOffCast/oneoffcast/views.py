eda0cfb10e44dfd1a0759d6b1833ac40fdaefb3a
Doug Hellmann <doug@doughellmann.com>
2006-09-17 15:28:49 +0000

    Add simiple user view with current queue; update main view with placeholder for popular podcasts
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@33 d63ba646-0c1d-0410-9968-bdc6b00dc62d

--- trunk/OneOffCast/oneoffcast/views.py
+++ trunk/OneOffCast/oneoffcast/views.py
@@ -11,14 +11,16 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 from django.contrib.auth.models import User	
 
-from oneoffcast.models import Podcast
+from oneoffcast.models import Podcast, QueueItem
 
 def main(request):
     """Main page for the app.
     """
     newest_podcasts = Podcast.objects.all().order_by('-registration_date')[:10]
+    popular_podcasts = Podcast.objects.all()[:10]
     return render_to_response('oneoffcast/index.html', 
                               {'newest_podcasts':newest_podcasts,
+                               'popular_podcasts':popular_podcasts,
                                })
 
 
@@ -27,6 +29,9 @@ def user(request, username):
     """Show information about the user.
     """
     if request.user.username == username:
-        return HttpResponse('user view for %s' % request.user.username)
+        queued_items = QueueItem.objects.filter(user=request.user).order_by('-add_date')
+        return render_to_response('oneoffcast/user.html',
+                                  {'queued_items':queued_items,
+                                   })
     else:
         raise RuntimeError('You are not allowed to see this page')
