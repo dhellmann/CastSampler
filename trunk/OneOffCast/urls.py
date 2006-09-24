43a0b409d5e0cd6d8f835fe4387a737005a01b17
Doug Hellmann <doug@doughellmann.com>
2006-09-24 21:47:50 +0000

    Add static content serving.
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@40 d63ba646-0c1d-0410-9968-bdc6b00dc62d

--- trunk/OneOffCast/urls.py
+++ trunk/OneOffCast/urls.py
@@ -4,21 +4,27 @@
 
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Example:
     # (r'^OneOffCast/', include('OneOffCast.apps.foo.urls.foo')),
-
+    
     # Main page
     (r'^$', 'OneOffCast.oneoffcast.views.main'),
-
+    
     # Override the URL for blind logins to take the user to their home page
     (r'^accounts/profile/$', 'OneOffCast.oneoffcast.views.login_redirect'),
     # Account registration, login, etc.
     (r'^accounts/', include('OneOffCast.registration.urls')),
     
     # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^admin/', include('django.contrib.admin.urls')),
 
     # The oneoffcast app
     (r'^cast/', include('OneOffCast.oneoffcast.urls')),
+
+    # Static content (CSS, images, etc.)
+    (r'^static/(.*)$', 'django.views.static.serve',
+     { 'document_root':'/Users/dhellmann/Devel/OneOffCast/src/trunk/OneOffCast/static'}),
+
 )
