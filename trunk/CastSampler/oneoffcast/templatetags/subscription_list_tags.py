09ebb287a3683892893d17e81d247f3072430428
Doug Hellmann <doug@doughellmann.com>
2006-11-24 23:39:01 +0000

    #84 - fix include templates to have access to user
     * `oneoffcast/templates/newest_subscription_list.html`:
    
       Only show the add button if the user is logged in. [[BR]]
    
     * `oneoffcast/templates/popular_subscription_list.html`:
    
       Only show the add button if the user is logged in. [[BR]]
    
     * `oneoffcast/templatetags/subscription_list_tags.py`:
    
       Recode the tags so they have access to context and can use the user
       variable. [[BR]]
    
    
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/CastSampler@163 d63ba646-0c1d-0410-9968-bdc6b00dc62d

--- trunk/CastSampler/oneoffcast/templatetags/subscription_list_tags.py
+++ trunk/CastSampler/oneoffcast/templatetags/subscription_list_tags.py
@@ -45,39 +45,70 @@ from oneoffcast.models import Podcast
 
 register = template.Library()
 
-@register.inclusion_tag('newest_subscription_list.html')
-def newest_subscription_list(count=5):
+class SubscriptionListNodeBase(template.Node):
+
+    def __init__(self, parser, token):
+        values = token.contents.split()
+        if len(values) == 1:
+            self.count = 5
+        elif len(values) == 2:
+            self.count = int(values[-1])
+        else:
+            raise ValueError('Tag takes only one argument')
+        return
+
+    def render(self, context):
+        raise NotImplementedError()
+        
+        
+
+class NewestSubscriptionNode(SubscriptionListNodeBase):
+    
+    def render(self, context):
+        newest_subscriptions = Podcast.objects.all().order_by('-registration_date')[:self.count]
+        context['newest_subscriptions'] = newest_subscriptions
+        t = template.loader.get_template('newest_subscription_list.html')
+        return t.render(context)
+
+@register.tag
+def newest_subscription_list(parser, token):
     """Show a list of the most recently added subscriptions.
     """
-    count = int(count)
-    newest_subscriptions = Podcast.objects.all().order_by('-registration_date')[:count]
-    return {'newest_subscriptions':newest_subscriptions,
-            }
+    node = NewestSubscriptionNode(parser, token)
+    return  node
 
-@register.inclusion_tag('popular_subscription_list.html')
-def popular_subscription_list(count=5):
+class PopularSubscriptionNode(SubscriptionListNodeBase):
+
+    def render(self, context):
+        #
+        # Look for how many individual episodes from a given subscription are referenced.
+        # Do we want to change this to count references to a subscription by a user
+        # instead of individual episodes?
+        #
+        popular_subscriptions_query = Podcast.objects.extra(
+            select={'use_count':"""select count(*)
+                                   from oneoffcast_queueitem
+                                   where oneoffcast_queueitem.podcast_id = oneoffcast_podcast.id
+                                   """,
+                    },
+            ).order_by('-use_count')
+        popular_subscriptions = []
+        for p in popular_subscriptions_query:
+            if p.use_count <= 0:
+                break
+            popular_subscriptions.append(p)
+            if len(popular_subscriptions) >= self.count:
+                break
+
+        context['popular_subscriptions']  = popular_subscriptions
+
+        t = template.loader.get_template('popular_subscription_list.html')
+        return t.render(context)
+
+
+@register.tag
+def popular_subscription_list(parser, token):
     """Show a list of the popular subscriptions based on individual show subscription count.
     """
-    count = int(count)
-
-    #
-    # Look for how many individual episodes from a given subscription are referenced.
-    # Do we want to change this to count references to a subscription by a user
-    # instead of individual episodes?
-    #
-    popular_subscriptions_query = Podcast.objects.extra(
-        select={'use_count':"""select count(*)
-                               from oneoffcast_queueitem
-                               where oneoffcast_queueitem.podcast_id = oneoffcast_podcast.id
-                               """,
-                },
-        ).order_by('-use_count')
-    popular_subscriptions = []
-    for p in popular_subscriptions_query:
-        if p.use_count <= 0:
-            break
-        popular_subscriptions.append(p)
-        if len(popular_subscriptions) >= count:
-            break
-
-    return { 'popular_subscriptions':popular_subscriptions }
+    node = PopularSubscriptionNode(parser, token)
+    return  node
